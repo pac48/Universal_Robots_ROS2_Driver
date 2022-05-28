5ce368edf6d069d7ea7b88756bc4cdf70b8fedb2
Paul Gesel <paul.gesel@picknik.ai>
2022-05-27 18:20:37 -0600

    my fork

--- ur_robot_driver/src/urcl_log_handler.cpp
+++ ur_robot_driver/src/urcl_log_handler.cpp
@@ -1,4 +1,4 @@
-// Copyright 2021 Universal Robots A/S
+// Copyright 2021 Universal Robots A/Swrite(
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
